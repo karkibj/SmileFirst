058b032b9bd292fba42be0f8d2b841a61289d4d4
karkibj <karkibj341@gmail.com>
2024-09-27 13:15:43 +0545

    updated

---
+++ backend/Server/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import messageRouter from './routes/getmessage.route.js'
+import cors from "cors";
+
+
+dotenv.config({
+    path:'../.env'
+})
+
+const app=express();
+app.use(cors({
+    origin:" http://localhost:3000",
+    methods:['POST']
+}))
+
+const PORT=4000
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser());
+app.use(messageRouter)
+
+
+app.listen(PORT,()=>{
+    console.log(`server running at port ${PORT}`)
+})
